ad156fc6b05e85b8ea5ebc93a01d7ed21bd197d6
Shun2710 <alexandr.prokoptsov@gmail.com>
2026-08-10 02:01:54 +0100

    Add search, categories and pagination

--- books/views.py
+++ books/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.db.models import Q
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 
-# Create your views here.
+from .models import Book, Category
+from .forms import BookForm
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'books/book_list.html'
+    context_object_name = 'books'
+    paginate_by = 5
+    ordering = ['id']
+
+    def get_queryset(self):
+        queryset = Book.objects.all()
+
+        search = self.request.GET.get('search')
+        if search:
+            queryset = queryset.filter(
+                Q(title__icontains=search) |
+                Q(author__icontains=search)
+            )
+
+        category = self.request.GET.get('category')
+        if category:
+            queryset = queryset.filter(category__slug=category)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'books/book_detail.html'
+    context_object_name = 'book'
+
+
+class BookCreateView(CreateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'books/book_form.html'
+    success_url = reverse_lazy('books:book_list')
+
+
+class BookUpdateView(UpdateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'books/book_form.html'
+    success_url = reverse_lazy('books:book_list')
+
+
+class BookDeleteView(DeleteView):
+    model = Book
+    template_name = 'books/book_confirm_delete.html'
+    success_url = reverse_lazy('books:book_list')
