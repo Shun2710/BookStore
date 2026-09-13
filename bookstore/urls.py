093e8588044846e4f0cec03ba6b770fe570fe92c
Shun2710 <alexandr.prokoptsov@gmail.com>
2026-09-13 19:30:06 +0100

    Add Ukrainian localization and language switcher

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
     path('__debug__/', include('debug_toolbar.urls')),
     path('accounts/', include('accounts.urls')),
     path('', include('books.urls')),
